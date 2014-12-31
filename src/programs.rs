5421fdfed53316ff8d70094f2d51947d0ca60a26
Michael Tremel <itrekkie@gmail.com>
2014-12-31 02:34:35 -0700

    reorganize

--- src/programs.rs
+++ src/programs.rs
@@ -10,6 +10,52 @@ pub struct Program {
   pub functions: Vec<::functions::Function>,
 }
 
+impl Program {
+  pub fn parse_textual_bytecode(path: Path) -> Result<Program, ParseError> {
+  let mut program = Program{functions: vec![Function{instructions: vec![]}]};
+  let mut file = BufferedReader::new(File::open(&path));
+
+  let mut instruction_count = 0u;
+  let mut jump_targets = HashMap::new();
+
+  for line in file.lines() {
+  let line_unwrap = try!(line);
+  if !line_unwrap.chars().all(|c| c.is_whitespace()) {
+    let mut it = line_unwrap.split_str(" ").map(|s| s.trim());
+    let opcode_it = try!(it.next().ok_or(ParseError::BadSplit));
+    let op = try!(Opcode::parse(opcode_it).ok_or(ParseError::BadOpcode));
+    let arg_it = try!(it.next().ok_or(ParseError::BadSplit));
+    let arg = try!(arg_it.parse::<int>().ok_or(ParseError::BadInt));
+
+    match op {
+      Opcode::Label => {
+        jump_targets.insert(arg, instruction_count);
+        },
+        _ => {
+          instruction_count += 1;
+          let new_instruction = Instruction{op: op, arg: arg};
+          program.functions.last_mut().unwrap().instructions.push(new_instruction);
+        }
+      }
+      } else {
+        // before moving onto a new function, normalize any instructions jump target
+        for instruction in program.functions.last_mut().unwrap().instructions.iter_mut() {
+          if instruction.op.is_jmp() {
+            instruction.arg = jump_targets[instruction.arg] as int;
+          }
+        }
+
+        instruction_count = 0;
+        jump_targets.clear();
+        program.functions.push(Function{instructions: vec![]});
+      }
+    }
+
+    Ok(program)
+  }
+}
+
+
 #[deriving(Show)]
 enum ParseError {
   Io(IoError),
@@ -22,50 +68,4 @@ impl FromError<IoError> for ParseError {
   fn from_error(err: IoError) -> ParseError {
     ParseError::Io(err)
   }
-}
-
-pub fn parse_program(path: Path) -> Result<Program, ParseError> {
-  let mut program = Program{functions: vec![Function{instructions: vec![]}]};
-  let mut file = BufferedReader::new(File::open(&path));
-
-  let mut instruction_count = 0u;
-  let mut jump_targets = HashMap::new();
-
-  for line in file.lines() {
-    let line_unwrap = try!(line);
-    if !line_unwrap.chars().all(|c| c.is_whitespace()) {
-      let mut it = line_unwrap.split_str(" ").map(|s| s.trim());
-      let opcode_it = try!(it.next().ok_or(ParseError::BadSplit));
-      let op = try!(Opcode::parse(opcode_it).ok_or(ParseError::BadOpcode));
-      let arg_it = try!(it.next().ok_or(ParseError::BadSplit));
-      let arg = try!(arg_it.parse::<int>().ok_or(ParseError::BadInt));
-
-      match op {
-        Opcode::Label => {
-          jump_targets.insert(arg, instruction_count);
-        },
-        _ => {
-          instruction_count += 1;
-          let new_instruction = Instruction{op: op, arg: arg};
-
-          let size = program.functions.len() - 1;
-          program.functions[size].instructions.push(new_instruction);
-        }
-      }
-    } else {
-      // before moving onto a new function, normalize any instructions jump target
-      let size = program.functions.len() - 1;
-      for instruction in program.functions[size].instructions.iter_mut() {
-        if instruction.op.is_jmp() {
-          instruction.arg = jump_targets[instruction.arg] as int;
-        }
-      }
-
-      instruction_count = 0;
-      jump_targets.clear();
-      program.functions.push(Function{instructions: vec![]});
-    }
-  }
-
-  Ok(program)
 }
