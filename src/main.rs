44ff2d3390e7e47cdc52ccd16522149c45dcb917
Michael Tremel <itrekkie@gmail.com>
2014-12-31 07:13:40 -0700

    clean

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@ mod tests {
   #[bench]
   fn bench_add_two(b: &mut Bencher) {
     let program = ::vm::ir::Program::parse_textual_bytecode(Path::new("test/fib.nornc")).unwrap();
-
     b.iter(|| ::vm::execute(&program));
   }
 }
