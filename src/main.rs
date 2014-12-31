1a22ffc8978c943a23ecf84174845adf7d491bb3
Michael Tremel <itrekkie@gmail.com>
2014-12-31 02:35:23 -0700

    fix compile error

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ mod instructions;
 mod functions;
 mod programs;
 
-use programs;
-
 fn main() {
-  let program = Program::parse_textual_bytecode(Path::new("test/fib.nornc")).unwrap();
+  let program = programs::Program::parse_textual_bytecode(Path::new("test/fib.nornc")).unwrap();
   println!("{}", program);
 }
