e0b6e5e4a5fb122b767f787b6bbc9ea167c36e84
Michael Tremel <itrekkie@gmail.com>
2015-01-04 14:26:21 -0700

    use args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 extern crate test;
 
+use std::os;
+
 mod vm;
 
 fn main() {
-  let program = vm::ir::Program::parse_textual_bytecode(Path::new("test/fib.nornc")).unwrap();
+  let args = os::args();
+  let args_tail = args.tail();
+  if args_tail.len() != 1 {
+    println!("Usage: norn_rust file.nornc");
+    return;
+  }
+
+  let file = &args_tail[0];
+  let program = vm::ir::Program::parse_textual_bytecode(Path::new(file)).unwrap();
+
   vm::execute(&program);
 }
 
