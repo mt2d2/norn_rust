2d0208016137e7c7a4442062aa7b6ebc67f2373d
Michael Tremel <itrekkie@gmail.com>
2015-04-04 02:21:48 -0700

    format

--- src/vm/frame.rs
+++ src/vm/frame.rs
@@ -2,17 +2,17 @@ use vm::ir::Function;
 use vm;
 
 pub struct Frame<'a> {
-  pub function: &'a Function,
-  pub ip: usize,
-  pub memory: [vm::Value; 1],
+    pub function: &'a Function,
+    pub ip: usize,
+    pub memory: [vm::Value; 1],
 }
 
 impl<'a> Frame<'a> {
-  pub fn new(function: &'a Function) -> Frame<'a> {
-    Frame{
-      function: function,
-      ip: 0,
-      memory: [0], // TODO you know the memory size required
+    pub fn new(function: &'a Function) -> Frame<'a> {
+        Frame{
+            function: function,
+            ip: 0,
+            memory: [0], // TODO you know the memory size required
+        }
     }
-  }
 }
