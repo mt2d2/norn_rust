6956e15f6a91cb9a163a74dbca91c6be9dd8819e
Michael Tremel <itrekkie@gmail.com>
2015-04-04 02:22:08 -0700

    format

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use std::path::Path;
 mod vm;
 
 fn main() {
-  let mut args = env::args();
-  if args.len() != 2 {
-    println!("Usage: norn_rust file.nornc");
-    return;
-  }
+    let mut args = env::args();
+    if args.len() != 2 {
+        println!("Usage: norn_rust file.nornc");
+        return;
+    }
 
-  let file = &args.nth(1).unwrap();
-  let program = vm::ir::Program::parse_textual_bytecode(Path::new(file)).unwrap();
+    let file = &args.nth(1).unwrap();
+    let program = vm::ir::Program::parse_textual_bytecode(Path::new(file)).unwrap();
 
-  vm::execute(&program);
+    vm::execute(&program);
 }
