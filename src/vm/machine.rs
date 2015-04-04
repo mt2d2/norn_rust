3d6e857d510c2210354ec49a03239cac71517eb8
Michael Tremel <itrekkie@gmail.com>
2015-04-04 02:24:29 -0700

    format

--- src/vm/machine.rs
+++ src/vm/machine.rs
@@ -5,102 +5,102 @@ use vm::ir::instructions::Opcode;
 use vm::Frame;
 
 pub fn execute(program: &Program) {
-  let main = program.functions.last().expect("No functions!");
+    let main = program.functions.last().expect("No functions!");
 
-  let mut frames = Vec::with_capacity(256);
-  let mut frame = &mut Frame::new(main);
+    let mut frames = Vec::with_capacity(256);
+    let mut frame = &mut Frame::new(main);
 
-  let mut stack = Vec::with_capacity(256);
+    let mut stack = Vec::with_capacity(256);
 
-  loop {
-    let instr = &frame.function.instructions[frame.ip];
-    frame.ip += 1;
+    loop {
+        let instr = &frame.function.instructions[frame.ip];
+        frame.ip += 1;
 
-    match instr.op {
-      Opcode::LitInt => {
-        stack.push(instr.arg);
-      },
+        match instr.op {
+            Opcode::LitInt => {
+                stack.push(instr.arg);
+            },
 
-      Opcode::LitChar => {
-        stack.push(instr.arg);
-      },
+            Opcode::LitChar => {
+                stack.push(instr.arg);
+            },
 
-      Opcode::StoreInt => {
-        frame.memory[instr.arg as usize] = stack.pop().unwrap();
-      },
+            Opcode::StoreInt => {
+                frame.memory[instr.arg as usize] = stack.pop().unwrap();
+            },
 
-      Opcode::StoreChar => {
-        frame.memory[instr.arg as usize] = stack.pop().unwrap();
-      },
+            Opcode::StoreChar => {
+                frame.memory[instr.arg as usize] = stack.pop().unwrap();
+            },
 
-      Opcode::LoadInt => {
-        stack.push(frame.memory[instr.arg as usize]);
-      },
+            Opcode::LoadInt => {
+                stack.push(frame.memory[instr.arg as usize]);
+            },
 
-      Opcode::LoadChar => {
-        stack.push(frame.memory[instr.arg as usize]);
-      },
+            Opcode::LoadChar => {
+                stack.push(frame.memory[instr.arg as usize]);
+            },
 
-      Opcode::PrintInt => {
-        print!("{}", stack.pop().unwrap());
-      },
+            Opcode::PrintInt => {
+                print!("{}", stack.pop().unwrap());
+            },
 
-      Opcode::PrintChar => {
-        print!("{}", stack.pop().unwrap() as u8 as char);
-      },
+            Opcode::PrintChar => {
+                print!("{}", stack.pop().unwrap() as u8 as char);
+            },
 
-      Opcode::LeInt => {
-        let a = stack.pop().unwrap();
-        let b = stack.pop().unwrap();
-        stack.push(if a < b { 1 } else { 0 });
-      },
+            Opcode::LeInt => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(if a < b { 1 } else { 0 });
+            },
 
-      Opcode::AddInt => {
-        let a = stack.pop().unwrap();
-        let b = stack.pop().unwrap();
-        stack.push(a + b);
-      },
+            Opcode::AddInt => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(a + b);
+            },
 
-      Opcode::SubInt => {
-        let a = stack.pop().unwrap();
-        let b = stack.pop().unwrap();
-        stack.push(a - b);
-      },
+            Opcode::SubInt => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(a - b);
+            },
 
-      Opcode::Tjmp => {
-        if stack.pop().unwrap() == 1 {
-          frame.ip = instr.arg as usize;
+            Opcode::Tjmp => {
+                if stack.pop().unwrap() == 1 {
+                    frame.ip = instr.arg as usize;
+                }
+            },
+
+            Opcode::Fjmp => {
+                if stack.pop().unwrap() == 0 {
+                    frame.ip = instr.arg as usize;
+                }
+            },
+
+            Opcode::Ujmp => {
+                frame.ip = instr.arg as usize;
+            },
+
+            Opcode::Call => {
+                let mut nframe = Frame::new(&program.functions[instr.arg as usize]);
+                mem::swap(frame, &mut nframe);
+                frames.push(nframe);
+            },
+
+            Opcode::Rtrn => {
+                if frames.is_empty() {
+                    break;
+                } else {
+                    let mut nframe = frames.pop().unwrap();
+                    mem::swap(frame, &mut nframe);
+                }
+            },
+
+            Opcode::Label => {
+                panic!("Unknown label encounted!");
+            },
         }
-      },
-
-      Opcode::Fjmp => {
-        if stack.pop().unwrap() == 0 {
-          frame.ip = instr.arg as usize;
-        }
-      },
-
-      Opcode::Ujmp => {
-        frame.ip = instr.arg as usize;
-      },
-
-      Opcode::Call => {
-        let mut nframe = Frame::new(&program.functions[instr.arg as usize]);
-        mem::swap(frame, &mut nframe);
-        frames.push(nframe);
-      },
-
-      Opcode::Rtrn => {
-        if frames.is_empty() {
-          break;
-        } else {
-          let mut nframe = frames.pop().unwrap();
-          mem::swap(frame, &mut nframe);
-        }
-      },
-
-      Opcode::Label => {
-        panic!("Unknown label encounted!");
-      },
     }
-  }
 }
